a914c476972e5bc9ba7c4a775083284e6d11ddab
Umesh <up9158256325@gmail.com>
2023-01-22 08:40:00 +0530

    Array list Imnplementation inside demo class

--- src/com/velocity/Demo.java
+++ src/com/velocity/Demo.java
@@ -1,10 +1,18 @@
 package com.velocity;
 
+import java.util.ArrayList;
+
 public class Demo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Adding a new Class");
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(95252);
+		
+		System.out.println(list);
 
 	}
 
