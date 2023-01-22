75d31a8d76b53bb8e8b3670815faf87ed7f4a656
Umesh <up9158256325@gmail.com>
2023-01-22 08:21:24 +0530

    Demo Project Done

---
+++ src/com/velocity/HashMapDemo.java
@@ -0,0 +1,11 @@
+package com.velocity;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello New Class Added after Stagging area");
+		
+	}
+
+}
