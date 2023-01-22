2ab1fc039d59944181e0eceaa02ba8170aa1765a
Umesh <up9158256325@gmail.com>
2023-01-22 08:27:54 +0530

    New Class Added

---
+++ src/com/velocity/Demo.java
@@ -0,0 +1,11 @@
+package com.velocity;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Adding a new Class");
+
+	}
+
+}
--- src/com/velocity/HashMapDemo.java
+++ src/com/velocity/HashMapDemo.java
@@ -1,11 +1,19 @@
 package com.velocity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMapDemo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello New Class Added after Stagging area");
+		Map<Integer,String> map = new HashMap<Integer,String>();
+		map.put(1,"Dilip");
+		map.put(2,"Rekha");
+		map.put(3,"Rajesh");
+		map.put(4,"Umesh");
 		
+		System.out.println(map);
 	}
 
 }
